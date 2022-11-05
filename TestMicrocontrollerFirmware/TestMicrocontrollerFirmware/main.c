b66fed1c12da40382b95bc5259441a1041b2aad3
Goochy12-Laptop <liamgooch@live.com.au>
2022-11-05 23:53:55 +1100

    create Test Microcontroller firmware package

---
+++ TestMicrocontrollerFirmware/TestMicrocontrollerFirmware/main.c
@@ -0,0 +1,18 @@
+/*
+ * TestMicrocontrollerFirmware.c
+ *
+ * Created: 5/11/2022 11:53:07 PM
+ * Author : liamg
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
